e612ac70ae541f81e1955506ceba09b5d583e435
Siddharth Yadav <siddharthyadav262005@gmail.com>
2026-02-26 00:18:54 +0530

    Prisma setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import { prisma } from "./prisma";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { identify } from "./controllers/identifyController";
 
 dotenv.config();
 
@@ -13,8 +15,15 @@ app.get("/", (req, res) => {
   res.send("Bitespeed Identity Service is Running");
 });
 
+app.get("/test-db", async (req, res) => {
+  const contacts = await prisma.contact.findMany();
+  res.json(contacts);
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.post("/identify", identify);
